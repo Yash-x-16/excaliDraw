86119f12563b6b95823faea782aa71dce4b20899
Yash Mishra <mishrayash392@gmail.com>
2025-07-02 12:59:27 +0530

    init

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,34 @@
 import express from "express" 
+import {JWT_SECRET} from "@repo/backend-common/config"
+import {client} from "@repo/db/client"
+
+
+    async function yash (){
+        try{
+        await client.user.create({
+            data:{
+                username:"yash",
+                password:"yash",
+                email:"yash1"
+            }
+        })
+        
+        console.log('USER CREATED')
+                
+}catch(e){
+    console.log("error in try block is ::",e)
+}
+
+        } 
+        
+        yash()
+        
 
 const app = express() 
 
 app.use(express.json())
 
-
+console.log(JWT_SECRET)
 app.listen(3002,()=>{
     console.log("port is running on 3002")
 })
