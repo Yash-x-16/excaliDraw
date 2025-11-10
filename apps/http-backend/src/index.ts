2559647cfc96e8da2b587abaee3460ce42fe4ccb
yash-x-16 <mishrayash392@gmail.com>
2025-11-10 20:18:34 +0530

    added @repo/backend-common/secret

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,4 +1,6 @@
-import express from "express" 
+import express from "express"  
+import {JWT_SECRET} from "@repo/backend-common/secret"
 const app = express() 
+console.log("jwt secret is : ",JWT_SECRET)
 
 app.listen()
