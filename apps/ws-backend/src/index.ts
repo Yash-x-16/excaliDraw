8f3c795764f374e1af1cde218f95e2cb4cfa40dc
yash-x-16 <mishrayash392@gmail.com>
2025-11-12 17:22:32 +0530

    fixed ws bug

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,3 +1,86 @@
-import {WebSocket} from "ws";
+import {WebSocket, WebSocketServer} from "ws";
 import {WS_PORT} from "@repo/backend-common/secret" 
+import { isTokenValid } from "./auth/auth"; 
+import {Chat}from "@repo/db/chatModel"
+import {connectDb} from "@repo/db/db"
+const wss = new WebSocketServer({port:WS_PORT}) 
+interface User{
+    roomId:string[] , 
+    userId:string , 
+    socket:WebSocket
+}
+let allUser:User[] = []
 
+wss.on("error",(e)=>{
+    console.log("error in the ws")
+}) ; 
+
+wss.on("connection",(socket,request)=>{
+    const url = request.url ; 
+    if(!url){
+        wss.close() 
+        return 
+    }
+    const queryParam = new URLSearchParams(url.split('?')[1]) ; 
+    const token = queryParam.get("token") ; 
+    if(!token){
+        wss.close()
+        return 
+    } 
+    const result = isTokenValid(token) ;  
+    if(result===null){
+        wss.close()
+        return 
+    }
+        
+    if(result){
+        allUser.push({
+            userId:result , 
+            roomId:[] , 
+            socket
+        })
+        socket.on("message",async(data)=>{
+            await connectDb()
+            const parsedMessage = JSON.parse(data.toString()) ; 
+            if(parsedMessage.type==="join"){
+                const user  = allUser.find(x=>x.socket===socket) ;  
+                console.log("user is : ",user) 
+                if(!user){
+                    return 
+                }
+                user.roomId.push(parsedMessage.roomId) ; 
+            }
+
+            if(parsedMessage.type==="chat"){
+                const message = parsedMessage.message ; 
+                const roomId = parsedMessage.roomId ; 
+                try {
+                    await Chat.create({
+                    userId:result , 
+                    roomId , 
+                    text:message
+                })
+                } catch (error) {
+                    console.log("error in connection in db",error)
+                }
+              
+                allUser.forEach(x=>{
+                    if(x.roomId.includes(roomId)){
+                        x.socket.send(JSON.stringify({
+                            type:"chat" , 
+                            message , 
+                            roomId
+                        }))
+                    }
+                })
+            }
+            if(parsedMessage.type==="leave"){
+                const user = allUser.find(x=>x.socket===socket) ; 
+                if (!user) {
+                    return
+                }
+                user.roomId = user.roomId.filter(x=>x!=parsedMessage.roomId) ; 
+            }
+        })
+    }
+})
