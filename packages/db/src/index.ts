dfb8d73b12fc1b2a8d3da107c6e49e77199c504a
yash-x-16 <mishrayash392@gmail.com>
2025-11-15 11:24:04 +0530

    reinit

--- packages/db/src/index.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import {DB_URL} from "@repo/backend-common/secret" 
-
-export const connectDb =async()=>{
-    const connection = await mongoose.connect(DB_URL) 
-    return connection.connection.host
-}
