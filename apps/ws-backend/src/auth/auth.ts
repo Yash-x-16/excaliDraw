123dddd470bf6811f9acae585f5ef74d9aed417c
yash-x-16 <mishrayash392@gmail.com>
2025-11-19 15:51:26 +0530

    added ws server

--- apps/ws-backend/src/auth/auth.ts
+++ apps/ws-backend/src/auth/auth.ts
@@ -1,16 +1,13 @@
-import jwt, { JwtPayload } from "jsonwebtoken" 
-import {JWT_SECRET} from "@repo/backend-common/secret" 
-
-export const checkAuth = (token:string):string |null=>{
+import { JWT_SECRET } from "@repo/backend-common/secret";
+import jwt, { JwtPayload } from "jsonwebtoken" ; 
+export const checkAuth = (token:string):string | null=>{ 
     try {
-        const decoded = jwt.verify(token as string ,JWT_SECRET) as JwtPayload ; 
+        const decoded = jwt.verify(token ,JWT_SECRET) as JwtPayload ; 
         if(decoded.userId){
-            return decoded.userId as string ; 
-        }else{
-            return null ; 
+            return decoded.userId as string 
         }
     } catch (error) {
-        console.log("Error in auth") ; 
         return null 
     }
+    return null
 }
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,77 @@
+import {WebSocket, WebSocketServer} from "ws"; 
+import {WS_PORT} from "@repo/backend-common/secret"
+import { checkAuth } from "./auth/auth";
+import {Chat} from "@repo/db/chat" 
+
+interface Users {
+    userId:string , 
+    roomId:string[] , 
+    socket:WebSocket
+}
+
+const wss = new WebSocketServer({port:WS_PORT}) ; 
+
+const allUsers:Users[] = []
+
+wss.on("error",(e)=>{
+    console.log("error in the ws server",e) ; 
+})
+
+wss.on("connection",async(socket,request)=>{
+    const url = request.url ; 
+    if(!url){
+        return 
+    }
+    const queryParam = new URLSearchParams(url?.split('?')[1]) 
+    const token = queryParam.get('token') ; 
+    if(!token){
+        wss.close() 
+        return ; 
+    }
+    const userId = checkAuth(token as string) ; 
+    socket.on("message",(data)=>{
+        if(userId){
+            allUsers.push({
+                userId , 
+                roomId:[] , 
+                socket
+            })
+            try {
+                const parsedMessage = JSON.parse(data.toString()) ; 
+                if(parsedMessage.type==="join"){
+                    const user = allUsers.find(x=>x.socket===socket) ; 
+                    if(!user){
+                        return ; 
+                    }
+                    user.roomId.push(parsedMessage.roomId);
+                }    
+                if(parsedMessage.type==="chat"){
+                    const message = parsedMessage.message ; 
+                    const roomId = parsedMessage.roomId ; 
+                    allUsers.filter(async x=>{
+                        await Chat.create({
+                            roomId , 
+                            text:message , 
+                            userId 
+                        })
+                        if(x.roomId.includes(roomId)){
+                            x.socket.send(message);
+                        }  
+                    })
+                }
+                if(parsedMessage.type==="leave"){
+                    const user = allUsers.find(x=>x.socket===socket) ; 
+                    if(!user){
+                        wss.close() 
+                        return ; 
+                    }
+                    user.roomId.filter(x =>x != parsedMessage.roomId) ; 
+                }
+            } catch (error) {
+                console.log("error in parsing json",error) ; 
+            }
+        }     
+        })
+    
+
+})
