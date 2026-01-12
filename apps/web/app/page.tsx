8d087a1240d901f90ee3d9efc7f9da291025ecf8
Yash_x_16 <mishrayash392@gmail.com>
2026-01-12 17:20:53 +0530

    added floating elements in the landing page

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { SketchElements } from "./component/ui/sketchElements";
+
+export default function Home() {
+  return (
+   <div className="min-h-screen overflow-hidden relative bg-black">
+    <SketchElements/>
+   </div>
+  );
+}
