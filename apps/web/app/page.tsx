d79b66e4ab3f1359cf9f744138220d14bdf8e990
Yash_x_16 <mishrayash392@gmail.com>
2026-01-12 17:42:24 +0530

    added virgil font

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,15 @@
 import { SketchElements } from "./component/ui/sketchElements";
-
+import Logo from "./component/ui/logo";
 export default function Home() {
   return (
    <div className="min-h-screen overflow-hidden relative bg-black">
-    <SketchElements/>
+    <SketchElements/> 
+  
+    {/* starting navbar ...*/}
+    <nav className="relative z-10">
+      <Logo className="text-purple-400"/> 
+      
+    </nav>
    </div>
   );
 }
