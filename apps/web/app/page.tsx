92da1c13b2264bf17f64f4e89cea7f903940def3
Yash_x_16 <mishrayash392@gmail.com>
2026-01-12 17:06:34 +0530

    trying to add tailwind as a package in the packages directory

--- apps/web/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import  SketchElements  from "@repo/ui/ui/icons"
-export default function Home() {
-  return <div className="min-h-screen overflow-hidden relative bg-black">
-   <SketchElements/>
-  </div>
-}
