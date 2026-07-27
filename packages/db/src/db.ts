5873432e60a5ea13012f0bd416a8cb4a0189cafd
Yash-x-16 <mishrayash392@gmail.com>
2026-07-27 23:39:10 +0530

    added db package

---
+++ packages/db/src/db.ts
@@ -0,0 +1,11 @@
+// src/lib/prisma.ts
+import { PrismaClient } from './generated/prisma/client' // Import from generated path
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+export const client = new PrismaClient({
+  adapter,
+})   
