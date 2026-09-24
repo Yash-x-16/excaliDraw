d7e1185c4c1e3aeb3124092db93c48c3858063fb
Yash-x-16 <mishrayash392@gmail.com>
2026-09-24 18:49:57 +0530

    completes ws server

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,5 +1,4 @@
-import  { WebSocketServer } from "ws" 
-import jwt, { decode, JwtPayload } from "jsonwebtoken"  
+import  { WebSocketServer ,WebSocket} from "ws" 
 import dotenv from "dotenv"  
 import {prisma} from "@repo/db/db"
 import { authMiddleware } from "./middleware"
@@ -8,6 +7,14 @@ dotenv.config()
 const port = process.env.WS_PORT ;  
 const wss = new WebSocketServer({port:Number(port)}) 
 
+
+interface allUsers {
+    userId :string , 
+    roomId:Number | null , 
+    webSocket:WebSocket
+} 
+
+let allUser:allUsers[] = [] ; 
 
 wss.on("connection",(ws,request)=>{ 
     const url =  request.url 
@@ -19,12 +26,55 @@ wss.on("connection",(ws,request)=>{
     const queryParams = new URLSearchParams(url.split('?')[1]) ; 
     const token = queryParams.get("token") || "" 
     const userId = authMiddleware(token)?.userId 
-    if(!userId){
+    if(!userId){ 
+        ws.close() 
         return {
             message:"no userId found"
         }
     }
-    ws.on("message",(data)=>{
+    allUser.push({
+        webSocket:ws  , 
+        userId :userId as string , 
+        roomId:null ,
+    })
+    ws.on("message",async(message)=>{
+        const data = JSON.stringify(message) ; 
+        const parsedData = JSON.parse(data) ; 
+        const roomId = Number(parsedData.roomId)
+        if(parsedData.type=="join"){
+            const user = allUser.find(x=>x.webSocket == ws) ; 
+            if(!user){ 
+                ws.close(404)
+                return
+            }
+            user.roomId = roomId ; 
+        }
 
+        if(parsedData.type=="leave"){
+            const user = allUser.find(x=>x.roomId == roomId) 
+            if(!user){
+                ws.close() 
+                return 
+            } 
+            user.roomId = null ; 
+        }
+
+        if(parsedData.type=="chat"){ 
+             const message = parsedData.message ; 
+            await prisma.chat.create({
+                data:{
+                    roomId , 
+                    message ,
+                    userId
+                }
+            })
+            allUser.forEach(user=>{
+                if(user.roomId==roomId){
+                    ws.send(JSON.stringify({ 
+                        message
+                    }))
+                }
+            })
+        }
     })
 })
